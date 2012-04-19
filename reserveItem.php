08589d9c08f720d5785e9f0b6d9410f0a2f668f9
Eugenia Korn <gina.korn@gmail.com>
2012-04-18 23:09:33 -0700

    reserve item Page, I am working out the bugs with the form

--- reserveItem.php
+++ reserveItem.php
@@ -13,54 +13,37 @@
 	$page_title = 'MHIS - Reserve-Item';
 	include ('./includes/header.html');
 	
-	
+	echo '<h1>Item Reservations</h1>';
 ?>	
 
 <form action="reserveItem.php" method="post">
 	<fieldset class = "forms">
 		<?php		
-			
-			//get the starting time and date
-			//get class into the page
-			require_once('includes/tc_calendar.php');
-			$date1_default = "2012-04-09";
-			$date2_default = "2012-04-15";
-			
-			
-			$myCalendar = new tc_calendar("date1", true);
-			$myCalendar->setIcon("images/iconCalendar.gif");
-			$myCalendar->setDate(date('d', strtotime($date1_default))
-				, date('m', strtotime($date1_default))
-				, date('Y', strtotime($date1_default)));
-			// $myCalendar->setPath("./");
-			$myCalendar->setYearInterval(1970, 2020);
-			$myCalendar->setAlignment('left', 'bottom');
-			$myCalendar->setDatePair('date1', 'date2', $date2_default);
-			$myCalendar->writeScript();	  
-			
-			echo'</br>';
-			echo'</br>';
-			//get the ending time and date
-			$myCalendar = new tc_calendar("date2", true);
-			$myCalendar->setIcon("images/iconCalendar.gif");
-			$myCalendar->setDate(date('d', strtotime($date2_default))
-				, date('m', strtotime($date2_default))
-				, date('Y', strtotime($date2_default)));
-			// $myCalendar->setPath("/");
-			$myCalendar->setYearInterval(1970, 2020);
-			$myCalendar->setAlignment('left', 'bottom');
-			$myCalendar->setDatePair('date1', 'date2', $date3_default);
-			$myCalendar->writeScript();	  
-			
-			echo'</br>';
-			echo'</br>';
-			echo'</br>';
-			echo '<label for="name">Student ID </label><input type="text" name="resident_id" id="resident_id" value="';
+			echo '<label for="name">Student ID: </label><input type="text" name="resident_id" id="resident_id" value="';
 				   if(isset($_GET['name'])) echo $_GET['name']; echo'" />';
 			
-			echo'</br>';			
+			echo'</br>';
+			echo '</br>';
 			echo'<label for="item">Item Name: </label> <input type="text" name="item" id="item_name" value="';
 								if(isset($_GET['item'])) echo $_GET['item']; echo'" />';	   
+			echo '</br>';
+			echo '</br>';
+			
+			echo'
+			<script>
+			$(function() {
+			$( "#Start_date" ).datepicker();
+			$( "#End_Date" ).datepicker();
+			});
+			</script>
+			
+				Start Date: <input id="Start_date" type="text"> End Date: <input id="End_Date" type="text">
+			';
+		
+					
+				
+			
+
 				
 		?>
 	</fieldset>	
